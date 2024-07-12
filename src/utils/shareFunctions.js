d75aafd6c8766e49b42dc9ac4360efb2bca5be4b
kareem <kareem.abdelnabi.22@gmail.com>
2024-07-13 01:07:43 +0300

    fix

--- src/utils/shareFunctions.js
+++ src/utils/shareFunctions.js
@@ -6,12 +6,12 @@ export const shareOnWhatsApp = (quote) => {
   
   export const shareOnTwitter = (quote) => {
     const text = encodeURIComponent(`"${quote.content}" - ${quote.author}`);
-    const url = encodeURIComponent("http://localhost:3000"); 
+    const url = encodeURIComponent("https://karreemm.github.io/quote-generator"); 
     window.open(`https://twitter.com/intent/tweet?text=${text}&url=${url}`, "_blank");
   };
   
   export const shareOnFacebook = (quote) => {
-    const baseUrl = "https://QuoteGenerator.com";
+    const baseUrl = "https://karreemm.github.io/quote-generator";
     const quoteText = encodeURIComponent(`${quote.content} - ${quote.author}`);
     const shareUrl = encodeURIComponent(baseUrl);
     const facebookShareUrl = `https://www.facebook.com/sharer/sharer.php?u=${shareUrl}&quote=${quoteText}`;
@@ -22,12 +22,12 @@ export const shareOnWhatsApp = (quote) => {
 
 
 export const shareOnTelegram = (quote) => {
-    const shareUrl = `https://t.me/share/url?url=${encodeURIComponent("https://QuoteGenerator.com")}&text=${`${quote.content} - ${quote.author}`}`;
+    const shareUrl = `https://t.me/share/url?url=${encodeURIComponent("https://karreemm.github.io/quote-generator")}&text=${`${quote.content} - ${quote.author}`}`;
     window.open(shareUrl, '_blank');
   };
   
   export const shareOnLinkedIn = (quote) => {
-    const shareUrl = `https://www.linkedin.com/shareArticle?url=${encodeURIComponent("https://QuoteGenerator.com")}&title=${encodeURIComponent("Quote from " + quote.author)}&summary=${encodeURIComponent(`${quote.content} - ${quote.author}`)}`;
+    const shareUrl = `https://www.linkedin.com/shareArticle?url=${encodeURIComponent("https://karreemm.github.io/quote-generator")}&title=${encodeURIComponent("Quote from " + quote.author)}&summary=${encodeURIComponent(`${quote.content} - ${quote.author}`)}`;
     window.open(shareUrl, '_blank');
   };
   
